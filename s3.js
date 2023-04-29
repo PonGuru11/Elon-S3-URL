bce92380f702813edb8061a0766893f17830a93a
PonGuru11 <125974004+PonGuru11@users.noreply.github.com>
2023-04-29 19:21:36 +0530

    Update s3.js

--- s3.js
+++ s3.js
@@ -177,6 +177,12 @@ const bodyParser = require('body-parser');
 const app = express();
 const port = 5000;
 
+
+const cors = require('cors');
+
+app.use(express.json());
+app.use(cors());
+
 app.use(bodyParser.json());
 
 // Set up AWS S3 client
