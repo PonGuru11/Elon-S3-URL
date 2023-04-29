c696f78e02d570208455ce6175c3ef1af4ab1232
PonGuru11 <125974004+PonGuru11@users.noreply.github.com>
2023-04-29 11:48:32 +0530

    Add files via upload

---
+++ s3.js
@@ -0,0 +1,162 @@
+// const AWS = require('aws-sdk');
+// const fs = require('fs');
+
+// // Set up AWS S3 client
+// const s3 = new AWS.S3({
+//   accessKeyId: "AKIAXWEQD3QLRVOKH5WF",
+//   secretAccessKey: "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",
+// });
+
+// // Define the bucket and file name
+// const bucketName = 'elon-e-sign1';
+// const fileName = 'application.pdf';
+
+// // Read the file from the local path
+// const fileContent = fs.readFileSync('C:/Users/ADMIN/Downloads/application.pdf');
+
+// // Upload the file to S3 bucket
+// const uploadParams = {
+//   Bucket: bucketName,
+//   Key: fileName,
+//   Body: fileContent
+// };
+
+// const uploadResult = s3.upload(uploadParams).promise();
+// // console.log(`File uploaded to S3: ${uploadResult.Location}`);
+
+// // Get the public URL of the file
+// const urlParams = {
+//   Bucket: bucketName,
+//   Key: fileName,
+// };
+
+// s3.getSignedUrlPromise('getObject', urlParams)
+//   .then(url => console.log(`Public URL of the file: ${url}`))
+//   .catch(error => console.error(error));
+
+
+// {used code } 
+
+// const AWS = require('aws-sdk');
+// const fs = require('fs');
+
+// // Set up AWS S3 client
+// const s3 = new AWS.S3({
+//     accessKeyId: "AKIAXWEQD3QLRVOKH5WF",
+//       secretAccessKey: "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",
+// });
+
+// // Define the bucket and file name
+// const bucketName = 'elon-e-sign1';
+// const fileName = 'English-2023.pdf';
+
+// // Read the file from the local path
+// const fileContent = fs.readFileSync('C:/Users/ADMIN/Downloads/English-2023.pdf');
+
+// // Upload the file to S3 bucket
+// const uploadParams = {
+//   Bucket: bucketName,
+//   Key: fileName,
+//   Body: fileContent
+// };
+
+// const uploadResult = s3.upload(uploadParams).promise();
+// console.log(`File uploaded to S3: ${uploadResult.Location}`);
+
+// const urlParams = {
+//       Bucket: bucketName,
+//       Key: fileName,
+//     };
+// s3.getSignedUrlPromise('getObject', urlParams)
+//   .then(url => console.log(`Public URL of the file: ${url}`))
+//   .catch(error => console.error(error));
+
+// // Get the list of objects in the bucket
+// const listParams = {
+//   Bucket: bucketName,
+// };
+
+// s3.listObjectsV2(listParams, function(err, data) {
+//   if (err) {
+//     console.error(err);
+//   } else {
+//     console.log(`Number of files in the bucket: ${data.Contents.length}`);
+    
+//     // If there are more than 10 files, delete the oldest file
+//     if (data.Contents.length > 3) {
+//       // Sort the files by creation date in ascending order
+//       const sortedFiles = data.Contents.sort((a, b) => a.LastModified - b.LastModified);
+//       const oldestFile = sortedFiles[0];
+      
+//       // Delete the oldest file
+//       const deleteParams = {
+//         Bucket: bucketName,
+//         Key: oldestFile.Key,
+//       };
+      
+//       s3.deleteObject(deleteParams, function(err, data) {
+//         if (err) {
+//           console.error(err);
+//         } else {
+//           console.log(`Oldest file deleted: ${oldestFile.Key}`);
+//         }
+//       });
+//     }
+//   }
+// });
+
+const express = require('express');
+const AWS = require('aws-sdk');
+const fs = require('fs');
+const bodyParser = require('body-parser');
+const app = express();
+const port = 5000;
+
+app.use(bodyParser.json());
+
+// Set up AWS S3 client
+const s3 = new AWS.S3({
+    accessKeyId: "AKIAXWEQD3QLRVOKH5WF",
+          secretAccessKey: "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",
+});
+
+app.post('/upload', (req, res) => {
+  const { filePath, fileName, bucketName } = req.body;
+  
+  // Read the file from the local path
+  const fileContent = fs.readFileSync(filePath);
+  
+  // Upload the file to S3 bucket
+  const uploadParams = {
+    Bucket: bucketName,
+    Key: fileName,
+    Body: fileContent
+  };
+  
+  s3.upload(uploadParams, (err, data) => {
+    if (err) {
+      console.error(err);
+      return res.status(500).json({ message: 'Failed to upload file' });
+    }
+    
+    // Get the public URL of the file
+    const urlParams = {
+      Bucket: bucketName,
+      Key: fileName,
+    };
+
+    s3.getSignedUrl('getObject', urlParams, (err, url) => {
+      if (err) {
+        console.error(err);
+        return res.status(500).json({ message: 'Failed to get file URL' });
+      }
+      console.log(`File uploaded to S3: ${data.Location}`);
+      console.log(`Public URL of the file: ${url}`);
+      res.json({ message: 'File uploaded successfully', fileUrl: url });
+    });
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Server listening at http://localhost:${port}`)
+});
