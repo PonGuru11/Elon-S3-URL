79dfba579ff71ef87903c5bb14b8690c4fe1aca9
PonGuru11 <125974004+PonGuru11@users.noreply.github.com>
2023-04-29 18:11:45 +0530

    latest commit

--- s3.js
+++ s3.js
@@ -105,6 +105,71 @@
 //   }
 // });
 
+
+// {Working Code} // **********   //
+
+// const express = require('express');
+// const AWS = require('aws-sdk');
+// const fs = require('fs');
+// const bodyParser = require('body-parser');
+// const app = express();
+// const port = 5000;
+
+// app.use(bodyParser.json());
+
+// // Set up AWS S3 client
+// const s3 = new AWS.S3({
+//     accessKeyId: "AKIAXWEQD3QLRVOKH5WF",
+//           secretAccessKey: "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",
+// });
+
+// app.post('/upload', (req, res) => {
+//   const { filePath, fileName, bucketName } = req.body;
+  
+//   // Read the file from the local path
+//   const fileContent = fs.readFileSync(filePath);
+  
+//   // Upload the file to S3 bucket
+//   const uploadParams = {
+//     Bucket: bucketName,
+//     Key: fileName,
+//     Body: fileContent
+//   };
+  
+//   s3.upload(uploadParams, (err, data) => {
+//     if (err) {
+//       console.error(err);
+//       return res.status(500).json({ message: 'Failed to upload file' });
+//     }
+    
+//     // Get the public URL of the file
+//     const urlParams = {
+//       Bucket: bucketName,
+//       Key: fileName,
+//     };
+
+//     s3.getSignedUrl('getObject', urlParams, (err, url) => {
+//       if (err) {
+//         console.error(err);
+//         return res.status(500).json({ message: 'Failed to get file URL' });
+//       }
+//       console.log(`File uploaded to S3: ${data.Location}`);
+//       console.log(`Public URL of the file: ${url}`);
+//       res.json({ message: 'File uploaded successfully', fileUrl: url });
+//     });
+//   });
+// });
+
+// app.listen(port, () => {
+//   console.log(`Server listening at http://localhost:${port}`)
+// });
+// {Working Code} // **********   //
+
+
+const multer = require('multer');
+
+// Set up multer for file uploads
+const upload = multer({ dest: 'uploads/' });
 const express = require('express');
 const AWS = require('aws-sdk');
 const fs = require('fs');
@@ -120,29 +185,29 @@ const s3 = new AWS.S3({
           secretAccessKey: "H5Xxi1AtKecP3CNoGInPPWmYTvhrRbPDpgIYSrIx",
 });
 
-app.post('/upload', (req, res) => {
-  const { filePath, fileName, bucketName } = req.body;
-  
-  // Read the file from the local path
-  const fileContent = fs.readFileSync(filePath);
-  
+
+app.post('/upload', upload.single('file'), (req, res) => {
+  const { filename, mimetype, path } = req.file;
+  const bucketName = 'e-sign1';
+
   // Upload the file to S3 bucket
   const uploadParams = {
     Bucket: bucketName,
-    Key: fileName,
-    Body: fileContent
+    Key: filename,
+    Body: fs.createReadStream(path),
+    ContentType: mimetype
   };
-  
+
   s3.upload(uploadParams, (err, data) => {
     if (err) {
       console.error(err);
       return res.status(500).json({ message: 'Failed to upload file' });
     }
-    
+
     // Get the public URL of the file
     const urlParams = {
       Bucket: bucketName,
-      Key: fileName,
+      Key: filename,
     };
 
     s3.getSignedUrl('getObject', urlParams, (err, url) => {
@@ -158,5 +223,5 @@ app.post('/upload', (req, res) => {
 });
 
 app.listen(port, () => {
-  console.log(`Server listening at http://localhost:${port}`)
-});
+    console.log(`Server listening at http://localhost:${port}`)
+  });
